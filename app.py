a005b92d86773e9fb8ff17a537eec6b4d4c08e95
welingtonVinicius <72838805+welingtonVinicius@users.noreply.github.com>
2024-07-03 10:24:23 +0200

    cleaning

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
-from dotenv import load_dotenv
+from dotenv import import_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-@app.before_request
-def handle_json():
-    if request.is_json:
-        request.get_json()
 
 class Workflow(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,38 +18,42 @@ class Workflow(db.Model):
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
-    workflow_id = db.Column(db.Integer, db.ForeignKey('workflow.id'),
-        nullable=False)
+    workflow_id = db.Column(db.Integer, db.ForeignKey('workflow.id'), nullable=False)
+
+@app.before_request
+def ensure_json_request():
+    if request.is_json:
+        request.get_json()
 
 @app.route('/')
-def index():
+def welcome_message():
     return jsonify({"message": "Welcome to the Workflow Management System!"})
 
 @app.route('/workflows', methods=['GET', 'POST'])
-def manage_workflows():
+def workflows():
     if request.method == 'POST':
         data = request.get_json()
         new_workflow = Workflow(name=data['name'])
         db.session.add(new_workflow)
         db.session.commit()
         return jsonify({"message": "Workflow created", "workflow": data}), 201
-    elif request.method == 'GET':
+    else:
         workflows = Workflow.query.all()
         workflows_data = [{"id": workflow.id, "name": workflow.name} for workflow in workflows]
-        return jsonify(workflows_data)
+        return jsonify(workflows signal)
 
 @app.route('/tasks', methods=['GET', 'POST'])
-def manage_tasks():
+def tasks():
     if request.method == 'POST':
         data = request.get_json()
         new_task = Task(name=data['name'], workflow_id=data['workflow_id'])
         db.session.add(new_task)
         db.session.commit()
         return jsonify({"message": "Task created", "task": data}), 201
-    elif request.method == 'GET':
+    else:
         tasks = Task.query.all()
         tasks_data = [{"id": task.id, "name": task.name, "workflow_id": task.workflow_id} for task in tasks]
-        return jsonify(tasksData)
+        return jsonify(tasks_data) 
 
 if __name__ == '__main__':
     db.create_all()
