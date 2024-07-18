449eae3a70b07e94c9a12662b7eb9bedc7c94b39
welingtonVinicius <72838805+welingtonVinicius@users.noreply.github.com>
2024-07-18 18:15:38 +0200

    Develop models.py

---
+++ models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+db = SQLAlchemy()
+
+class Workflow(db.Model):
+    __tablename__ = 'workflows'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    description = Column(String(255), nullable=True)
+    status = Column(String(50), nullable=False)
+
+    tasks = relationship('Task', backref='workflow')
+
+class Task(db.Model):
+    __tablename__='tasks'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255), nullable=False)
+    description = Column(String(255), nullable=True)
+    workflow_id = Column(Integer, ForeignKey('workflows.id'), nullable=False)
+    status = Column(String(50), nullable=False)
