147c3e729105affa2c46cc788f241612b1038c84
welingtonVinicius <72838805+welingtonVinicius@users.noreply.github.com>
2024-07-19 19:43:09 +0200

    add new variables

--- models.py
+++ models.py
@@ -4,19 +4,19 @@ from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
-class Workflow(db.Model):
+class WorkflowModel(db.Model):
     __tablename__ = 'workflows'
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
     description = Column(String(255), nullable=True)
-    status = Column(String(50), nullable=False)
+    current_status = Column(String(50), nullable=False)
 
-    tasks = relationship('Task', backref='workflow')
+    associated_tasks = relationship('TaskModel', backref='associated_workflow')
 
-class Task(db.Model):
+class TaskModel(db.Model):
     __tablename__='tasks'
     id = Column(Integer, primary_key=True)
     title = Column(String(255), nullable=False)
     description = Column(String(255), nullable=True)
     workflow_id = Column(Integer, ForeignKey('workflows.id'), nullable=False)
-    status = Column(String(50), nullable=False)
+    current  = Column(String(50), nullable=False)
