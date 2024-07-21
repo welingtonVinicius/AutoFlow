d5131bacbc140992288be335b4aa9e2a51f25b94
welingtonVinicius <72838805+welingtonVinicius@users.noreply.github.com>
2024-07-21 06:13:27 +0200

    optimize the naming

--- models.py
+++ models.py
@@ -4,19 +4,19 @@ from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
-class WorkflowModel(db.Model):
+class Workflow(db.Model):
     __tablename__ = 'workflows'
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
     description = Column(String(255), nullable=True)
-    current_status = Column(String(50), nullable=False)
+    status = Column(String(50), nullable=False)
 
-    associated_tasks = relationship('TaskModel', backref='associated_workflow')
+    tasks = relationship('Task', backref='workflow')
 
-class TaskModel(db.Model):
-    __tablename__='tasks'
+class Task(db.Model):
+    __tablename__ = 'tasks'
     id = Column(Integer, primary_key=True)
     title = Column(String(255), nullable=False)
     description = Column(String(255), nullable=True)
     workflow_id = Column(Integer, ForeignKey('workflows.id'), nullable=False)
-    current  = Column(String(50), nullable=False)
+    status = Column(String(50), nullable=False)
