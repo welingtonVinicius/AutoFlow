91f134f83857973ad5e67e4f3994051fc4b7cd08
welingtonVinicius <72838805+welingtonVinicius@users.noreply.github.com>
2024-07-31 09:23:43 +0200

    change variable names

--- db.py
+++ db.py
@@ -5,32 +5,31 @@ import os
 
 load_dotenv()
 
-DATABASE_USER = os.getenv('DATABASE_USER')
-DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
-DATABASE_HOST = os.getenv('DATABASE_HOST')
-DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')
-DATABASE_NAME = os.getenv('DATABASE_NAME')
+DB_USER = os.getenv('DATABASE_USER')
+DB_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DB_HOST = os.getenv('DATABASE_HOST')
+DB_PORT = os.getenv('DATABASE_PORT', '5432')
+DB_NAME = os.getenv('DATABASE_NAME')
 
-DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+DB_CONNECTION_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 try:
-    engine = create_engine(DATABASE_URL)
-    # Test connection
-    with engine.begin() as conn:
-        conn.execute("SELECT 1")
-except exc.SQLAlchemyError as e:
-    print(f"Failed to connect to the database: {e}")
+    database_engine = create_engine(DB_CONNECTION_URL)
+    with database_engine.begin() as connection:
+        connection.execute("SELECT 1")
+except exc.SQLAlchemyError as error:
+    print(f"Failed to connect to the database: {error}")
     raise
 
-Session = sessionmaker(bind=engine)
+DatabaseSession = sessionmaker(bind=database_engine)
 
-def get_db_connection():
-    session = Session()
+def get_database_session():
+    session = DatabaseSession()
     try:
         yield session
-    except exc.SQLAlchemyError as e:
+    except exc.SQLAlchemyError as error:
         session.rollback()
-        print(f"Database session error: {e}")
+        print(f"Database session error: {error}")
         raise
     finally:
         session.close()
